0fc0b9053972e46dbb7f2bba851c69cec29c7125
Fedor Boretskiy <fedor.boretsky@gmail.com>
2020-09-13 20:27:48 +0300

    Added model

--- AR Webinar MDA/ViewController.swift
+++ AR Webinar MDA/ViewController.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        // Turn On face recognition – Does'nt work on iPhone 6s +
+//        let configuration = ARFaceTrackingConfiguration()
+//        arView.session.run(configuration)
+        
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let boxAnchor = try! ARWebinar.loadScene()
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
